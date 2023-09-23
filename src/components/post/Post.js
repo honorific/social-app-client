29423c330f0ba8dbdcf545021a026409b8c44aca
honorific <honorific.info@gmail.com>
2023-09-23 10:19:12 +0330

    styling post skeleton

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,4 +1,5 @@
 import './post.css'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
 
 const Post = () => {
   return (
@@ -7,8 +8,12 @@ const Post = () => {
         <div className='postTop'>
           <div className='postTopLeft'>
             <img className='postProfileImg' src='/assets/person/1.jpeg' />
+            <span className='postUsername'>honorific</span>
+            <span className='postDate'>5 minutes ago</span>
           </div>
-          <div className='postTopRight'></div>
+          <div className='postTopRight'>
+            <MoreVertIcon />
+          </div>
         </div>
         <div className='postCenter'></div>
         <div className='postBottom'></div>
