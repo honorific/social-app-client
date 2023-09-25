abe08fc0e1c4e2254662f4803edb000b65a5e9a8
honorific <honorific.info@gmail.com>
2023-09-25 08:51:28 +0330

    jsx after ad in rightbar

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -12,6 +12,15 @@ const Rightbar = () => {
           </span>
         </div>
         <img className='rightbarAd' src='/assets/ad.png' />
+        <h4 className='rightbarTitle'>Online Friends</h4>
+        <ul className='rightbarFriendList'>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img className='rightbarProfileImg' src='assets/person/3.jpg' />
+              <span className='rightbarOnline'></span>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   )
