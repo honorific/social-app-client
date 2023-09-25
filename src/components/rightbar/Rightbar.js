53103a6807b1501d9ae915fe9b0a073bbf388668
honorific <honorific.info@gmail.com>
2023-09-25 08:26:15 +0330

    right bar birthday jsx

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -1,7 +1,18 @@
 import './rightbar.css'
 
 const Rightbar = () => {
-  return <div className='rightbar'>Rightbar</div>
+  return (
+    <div className='rightbar'>
+      <div className='rightbarWrapper'>
+        <div className='birthdayContainer'>
+          <img className='birthdayImg' src='/assets/gift.png' />
+          <span className='birthdayText'>
+            <b>Pola foster </b> <b>and 3 friends </b> have a birthday today.
+          </span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Rightbar
