494e11a42a6052fe0695097b435f1a072e4d442c
honorific <honorific.info@gmail.com>
2023-10-05 19:45:37 +0330

    make post component dynamic

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,32 +1,32 @@
 import './post.css'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 
-const Post = () => {
+const Post = ({post}) => {
   return (
     <div className='post'>
       <div className='postWrapper'>
         <div className='postTop'>
           <div className='postTopLeft'>
-            <img className='postProfileImg' src='/assets/person/1.jpeg' />
+            <img className='postProfileImg' src={post.photo} />
             <span className='postUsername'>honorific</span>
-            <span className='postDate'>5 minutes ago</span>
+            <span className='postDate'>{post.date}</span>
           </div>
           <div className='postTopRight'>
             <MoreVertIcon />
           </div>
         </div>
         <div className='postCenter'>
-          <span className='postText'>Hey its my first post</span>
+          <span className='postText'>{post?.desc}</span>
           <img className='postImg' src='/assets/post/1.jpeg' />
         </div>
         <div className='postBottom'>
           <div className='postBottomLeft'>
             <img className='likeIcon' src='assets/like.png' />
             <img className='likeIcon' src='assets/heart.png' />
-            <span className='postLikeCounter'>32 people like it</span>
+            <span className='postLikeCounter'>{post.like} people like it</span>
           </div>
           <div className='postBottomRight'>
-            <span className='postCommentText'>9 comments</span>
+            <span className='postCommentText'>{post.comment} comments</span>
           </div>
         </div>
       </div>
