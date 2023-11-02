f8a87c3b7645f0f0e8375937775235fbcf288dc7
honorific <honorific.info@gmail.com>
2023-11-02 09:39:47 +0330

    make name and profile pic dynamic for online users

--- src/components/online/Online.js
+++ src/components/online/Online.js
@@ -4,10 +4,10 @@ const Online = ({user}) => {
   return (
     <li className='rightbarFriend'>
       <div className='rightbarProfileImgContainer'>
-        <img className='rightbarProfileImg' src='assets/person/3.jpeg' />
+        <img className='rightbarProfileImg' src={user.profilePicture} />
         <span className='rightbarOnline'></span>
       </div>
-      <span className='rightbarUsername'>John doe</span>
+      <span className='rightbarUsername'>{user.username}</span>
     </li>
   )
 }
