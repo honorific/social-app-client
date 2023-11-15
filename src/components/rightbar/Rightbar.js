ba5575c62203abd8e68f39de82871b82eff974c7
honorific <honorific.info@gmail.com>
2023-11-15 21:36:51 +0330

    different types of rightbar

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -1,10 +1,10 @@
 import './rightbar.css'
 import {Users} from '../../dummyData'
 import Online from '../online/Online'
-const Rightbar = () => {
-  return (
-    <div className='rightbar'>
-      <div className='rightbarWrapper'>
+const Rightbar = ({profile}) => {
+  const HomeRightbar = () => {
+    return (
+      <>
         <div className='birthdayContainer'>
           <img className='birthdayImg' src='/assets/gift.png' />
           <span className='birthdayText'>
@@ -19,6 +19,16 @@ const Rightbar = () => {
             return <Online key={u.id} user={u} />
           })}
         </ul>
+      </>
+    )
+  }
+  const ProfileRightbar = () => {
+    return <h4>User information</h4>
+  }
+  return (
+    <div className='rightbar'>
+      <div className='rightbarWrapper'>
+        <ProfileRightbar />
       </div>
     </div>
   )
