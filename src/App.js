01a33d852f54f4fe7efe53b3ce17295a5b76e32f
honorific <honorific.info@gmail.com>
2023-12-26 21:26:10 +0330

    initialize routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Profile from './pages/profile/Profile'
 import Register from './pages/register/Register'
+
 const App = () => {
-  return <Home />
+  const router = createBrowserRouter(
+    createRoutesFromElements([
+      <Route path='/' element={<Home />} />,
+      <Route path='/login' element={<Login />} />,
+      <Route path='/register' element={<Register />} />,
+      <Route path='/profile/:username' element={<Profile />} />,
+    ]),
+  )
+  return <RouterProvider router={router} />
 }
 
 export default App
