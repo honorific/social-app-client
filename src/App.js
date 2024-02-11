717d78b8b0aa78653aea2df55dba628fa2d4d404
honorific <honorific.info@gmail.com>
2024-02-11 20:54:40 +0330

    manage redirects of routes

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Profile from './pages/profile/Profile'
 import Register from './pages/register/Register'
+import {useContext} from 'react'
 
 const App = () => {
+  
   const router = createBrowserRouter(
     createRoutesFromElements([
       <Route path='/' element={<Home />} />,
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -3,6 +3,7 @@ import './login.css'
 import {loginCall} from '../../apiCalls'
 import {AuthContext} from '../../context/AuthContext'
 import {CircularProgress} from '@mui/material'
+import {Navigate, redirect} from 'react-router-dom'
 
 const Login = () => {
   const {user, isFetching, error, dispatch} = useContext(AuthContext)
@@ -12,50 +13,58 @@ const Login = () => {
     e.preventDefault()
     loginCall({email, password}, dispatch)
   }
-  console.log('user is: ', user)
   return (
-    <div className='login'>
-      <div className='loginWrapper'>
-        <div className='loginLeft'>
-          <h3 className='loginLogo'>Social App</h3>
-          <span className='loginDesc'>
-            Connect with friends and the world arround you on Social App
-          </span>
-        </div>
-        <div className='loginRight'>
-          <form className='loginBox' onSubmit={handleClick}>
-            <input
-              placeholder='Email'
-              className='loginInput'
-              required
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              placeholder='Password'
-              className='loginInput'
-              type='password'
-              required
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button className='loginButton' disabled={isFetching}>
-              {isFetching ? (
-                <CircularProgress sx={{color: 'white', mt: '5px'}} size={25} />
-              ) : (
-                'Log In'
-              )}
-            </button>
-            <span className='loginForgot'>Forgot Password</span>
-            <button className='loginRegisterButton'>
-              {isFetching ? (
-                <CircularProgress sx={{color: 'white', mt: '5px'}} size={25} />
-              ) : (
-                'Create a New Account'
-              )}
-            </button>
-          </form>
+    <>
+      {user && <Navigate to='/' />}
+      <div className='login'>
+        <div className='loginWrapper'>
+          <div className='loginLeft'>
+            <h3 className='loginLogo'>Social App</h3>
+            <span className='loginDesc'>
+              Connect with friends and the world arround you on Social App
+            </span>
+          </div>
+          <div className='loginRight'>
+            <form className='loginBox' onSubmit={handleClick}>
+              <input
+                placeholder='Email'
+                className='loginInput'
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                placeholder='Password'
+                className='loginInput'
+                type='password'
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button className='loginButton' disabled={isFetching}>
+                {isFetching ? (
+                  <CircularProgress
+                    sx={{color: 'white', mt: '5px'}}
+                    size={25}
+                  />
+                ) : (
+                  'Log In'
+                )}
+              </button>
+              <span className='loginForgot'>Forgot Password</span>
+              <button className='loginRegisterButton'>
+                {isFetching ? (
+                  <CircularProgress
+                    sx={{color: 'white', mt: '5px'}}
+                    size={25}
+                  />
+                ) : (
+                  'Create a New Account'
+                )}
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
