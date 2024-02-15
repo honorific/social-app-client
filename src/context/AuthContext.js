61acbfad715187d0c0a26770aa3a9b57fe673993
honorific <honorific.info@gmail.com>
2024-02-15 20:43:16 +0330

    set predefind info \rmation for not login each time

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,7 +2,16 @@ import {createContext, useReducer} from 'react'
 import AuthReducer from './AuthReducer'
 
 const INITIAL_STATE = {
-  user: null,
+  user: {
+    _id: '65cbbda36f480c9038fbc01a',
+    username: 'taha',
+    email: 'taha@test.com',
+    profilePicture: '',
+    coverPicture: '',
+    isAdmin: false,
+    followers: Array(empty),
+    followings: Array(empty),
+  },
   isFetching: false,
   error: false,
 }
