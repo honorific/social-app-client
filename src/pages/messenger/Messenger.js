7d5ea37cdc4c78de73d823910e20bf9402074a03
honorific <honorific.info@gmail.com>
2024-02-28 21:29:26 +0330

    skeleton compeletion

--- src/pages/messenger/Messenger.js
+++ src/pages/messenger/Messenger.js
@@ -10,7 +10,17 @@ const Messenger = () => {
     <>
       {user._id && <Navigate to='/' />}
       <Topbar />
-      <div className='messenger'>hello</div>
+      <div className='messenger'>
+        <div className='chatMenu'>
+          <div className='chatMenuWrapper'>menu</div>
+        </div>
+        <div className='chatBox'>
+          <div className='chatBoxWrapper'>box</div>
+        </div>
+        <div className='chatOnline'>
+          <div className='chatOnlineWrapper'>online</div>
+        </div>
+      </div>
     </>
   )
 }
